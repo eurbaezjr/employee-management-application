952602669cf633776b5309956c1d7aabadbb9844
Eduardo Urbaez <eurbaezjr@gmail.com>
2019-12-07 21:27:10 -0800

    final upload

---
+++ Develop/app.js
@@ -0,0 +1,107 @@
+const employee = require("./lib/Employee.js");
+const engineer = require("./lib/Engineer.js");
+const manager = require("./lib/Manager.js");
+const intern = require("./lib/Intern.js");
+const render  = require("./lib/render.js");
+var path = require('path');
+const fs = require('fs');
+const inquirer = require("inquirer");
+var managerFirst = false;
+var teamMembers = [];
+const outputFile = path.resolve(__dirname, "output", "team.html");
+const questions = [
+{
+    type: 'list',
+    message: "Select the employee's role",
+    name: "title",
+    choices: ['Manager', 'Engineer', 'Intern', "I don't want to add more team members"]
+},    
+{
+    message: "Enter employee's full name",
+    name: "name"
+},
+{
+    message: "Enter employee's id",
+    name: "id"
+},
+{
+    message: "Enter employee's email",
+    name: "email"
+},
+];
+
+const officeQuestion = {
+    message: "Enter the manager's office number",
+    name: "officeNumber"
+};
+
+const gitQuestion = {
+    message: "Enter the engineer's GitHub username",
+    name: "github"
+};
+
+const schoolQuestion = {
+    message: "Enter the intern's school name",
+    name: "school"
+};
+
+
+function start() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers.name);
+            let name = answers.name;
+            let id = answers.id;
+            let title = answers.title;
+            let email = answers.email;
+            if (title == "Manager" && managerFirst == false) {
+                managerFirst = true;
+                inquirer
+                    .prompt(officeQuestion)
+                    .then(answer => {
+                        let officeNumber = answer.officeNumber;
+                        var boss = new Manager(name, id, email, officeNumber);
+                        teamMembers.push(boss);
+                        start();
+                    });
+            }
+            else if (title == "Manager" && managerFirst == true) {
+                console.log("Manager info can be added only once")
+                start();
+            }
+            else
+                if (title == "Engineer") {
+                    inquirer
+                        .prompt(gitQuestion)
+                        .then(answer => {
+                            let github = answer.github;
+                            var engineers = new Engineer(name, id, email, github);
+                            teamMembers.push(engineers);
+                            start();
+                        });
+                }
+                else
+                    if (title == "Intern") {
+                        inquirer
+                            .prompt(schoolQuestion)
+                            .then(answer => {
+                               const school = answer.school;
+                               var interns = new Intern(name, id, email, school);
+                                teamMembers.push(interns);
+                                start();
+                            });
+                    }
+                    else if(title == "I don't want to add more team members"){
+                        buildTeam();
+                    }
+
+        }); // end of inquirer
+
+        function buildTeam(){
+            fs.writeFileSync(outputFile, render(teamMembers), "utf-8")
+        }
+}
+start();
+
+
