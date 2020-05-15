015ee6fe288d611fcacf2d3db82093ba2c45c088
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-05-15 12:09:00 -0700

    included additional comments

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,4 +1,4 @@
-// Engineer constructor class uses the reusable employee module to maintain dry principle
+// Engineer constructor class uses the reusable employee module to maintain the dry principle
 const Employee = require("./Employee.js")
 class Engineer extends Employee {
     constructor(name, id, email, github) {
